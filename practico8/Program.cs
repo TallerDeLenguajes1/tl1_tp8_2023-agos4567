3b1f378aaa156d337006de7eb860418f20cee295
agos4567 <agosgalfre23@gmail.com>
2023-07-12 14:09:27 -0300

    punto2

--- practico8/Program.cs
+++ practico8/Program.cs
@@ -1,150 +1,83 @@
-﻿// using System;
-// using System.Collections.Generic;
-// using ClasesGeneral;
-// using System.Collections.Generic;
-// using System.Text;
-
-// // See https://aka.ms/new-console-template for more information
-
-
-// // Console.WriteLine("Hello, World!");
-  
-
-//   Random random = new Random();
-
-//   int cantPend= random.Next(2,4);
-
-// // bool condicion =false;
-//   List<Tarea> listaDeTareasPendientes = new List<Tarea>();
-
-
-//   List<Tarea> listaDeTareasR = new List<Tarea>();
-
-//   Tarea aux = new Tarea();
-
-
-//   int opcion =7;
+﻿using System;
+using System.IO;
+using  System.Collections.Generic;//para poder trabajar con listas genericas
 
 
 
+namespace  ObjectDirectory
 
+   
+{
 
-//      for (int i = 0; i < cantPend; i++)
-//      {
-//         Console.WriteLine(" //// Carga de Tareas: ////");
-//         Console.WriteLine("Ingrese la descripcion de la tarea: ");
-//         string? auxTarea = Console.ReadLine();
-//          Console.WriteLine("Ingrese la duracion ");
-//           int auxDuracion=Convert.ToInt32(Console.ReadLine());
+   class Program
+   {
+    static void Main(string[] args)
+    {
 
-//          Tarea tarea= new Tarea();
-//               tarea.IDtarea = i+ 1;
-//               tarea.Descripcion=auxTarea;
+       Console.WriteLine("Ingrese la ruta de la carpeta:");
+       string? rutaCarpeta=Console.ReadLine();
 
-//           if (auxDuracion<10 || auxDuracion>100)
-//           {
-//              do
-//             {
-//       Console.WriteLine("Ingrese la duracion nuevamente (entre 10/100)");
-//        auxDuracion=Convert.ToInt32(Console.ReadLine());
-
-
-              
-//             } while(auxDuracion<10 || auxDuracion>100);
-          
-//           }
-
-         
-         
-//   if(auxDuracion>=10 || auxDuracion <=100)
-//          {
-           
-//            tarea.Duracion= auxDuracion;
-//           }
-
-         
-//               listaDeTareasPendientes.Add(tarea);
-//        }
-
-        
-//        do
-//        {
-        
-//             Console.WriteLine("(1)--MOSTRAR TAREAS PENDIENTES");
-//             Console.WriteLine("(2)--MOSTRAR TAREAS REALIZADAS");
-//              Console.WriteLine("(3)--MOVER--");
-//              Console.WriteLine("(4)--Buscar por palabra");
-//              Console.WriteLine("(5)--Buscar por id");
-//               Console.WriteLine("(0)--Cerrar");
-
-//                opcion=Convert.ToInt32(Console.ReadLine());
-
-                
-//            switch (opcion)
-//            {
-//             case 1:
     
-//       Console.WriteLine("----Tareas pendientes-----");
-//       Funciones.mostrarTareas(listaDeTareasPendientes);
-       
+      string[] Archivos=Directory.GetFiles(rutaCarpeta);
 
 
-//             break;
+        Console.WriteLine("- - - Archivos econtrados - - -");
+          foreach (string archivo in Archivos)
+        
+          {
+
+            string nomArchvios=Path.GetFileNameWithoutExtension(archivo);
+            string ext=Path.GetExtension(archivo);
+            Console.WriteLine($" {nomArchvios} ({ext})");
+            
+            //   Console.WriteLine($"Nombre: {Path.GetFileName(archivo)
+            // }   - {Path.GetExtension(archivo)}");
+            //     // Console.WriteLine($"Nombre: {Path.GetFileName(archivo)
+            // } 
+            
+            //  ");
 
 
-//           case 2:
+            
 
-//             Console.WriteLine("----Tareas realizadas-----");
-//         Funciones.mostrarTareas(listaDeTareasR);
+          }
+           
+              //guarde en un archivo csv (archivos separados por comas) llamado “index.csv”
 
+            
+           string ruteCvs=  Path.Combine(@"c:\Repogit\tp8\tl1_tp8_2023-agos4567\practico8", "index.csv");
+          Console.WriteLine("Listado de archivos guardado en index.csv");
+  
+     
+   
+ 
+                  using (StreamWriter writer =new StreamWriter (ruteCvs))
+                  {
+                    writer.WriteLine("indice -nombre - extencion");
 
-//             break;
+                   for (int i = 0; i < Archivos.Length; i++)
+                   {
+                     
+                     string archivoNombre=Path.GetFileNameWithoutExtension(Archivos[i]);
+                     string archivoExt=Path.GetExtension(Archivos[i]);
+                    
+                     writer.WriteLine($"{i +1}, {archivoNombre}, {archivoExt}");
 
-//          case 3:
+                   }
 
-//     Funciones.MoverTarea(listaDeTareasPendientes,listaDeTareasR);
-//          break;
+ 
 
+                  }  
 
+ 
 
-
-//             case 4:
-
-//             Console.WriteLine("Ingrese la clave de la tarea a buscar: ");
-//             string? clave=Console.ReadLine();
-
-              
-//         aux=Funciones.BuscarTareaClave(clave, listaDeTareasPendientes, listaDeTareasR);
-//               Console.WriteLine("---Tarea encontrada---");
-//             Console.WriteLine("Id de la tarea:" +aux.IDtarea);
-//             Console.WriteLine("Descripcion: " + aux.Descripcion);
-//         Console.WriteLine("Duracion: " + aux.Duracion);
-             
-
-//             break;
-
-//             case 5:
-
-//             Console.WriteLine("Ingrese el id de la tarea a buscar:");
-//               int idbuscar=Convert.ToInt32(Console.ReadLine());
-
-//                 aux=Funciones.BuscarTareaId(idbuscar, listaDeTareasPendientes, listaDeTareasR);
-                  
-              
-//             Console.WriteLine("Id de la tarea:" +aux.IDtarea);
-         
-//         Console.WriteLine("Descripcion: " + aux.Descripcion);
-//         Console.WriteLine("Duracion: " + aux.Duracion);
-//             break;
-//             default:
-//             break;
-//            }
+    }
 
 
+ 
+          }
 
 
-//        } while (opcion !=0);
+           }
 
-
-// Funciones.Sumario(listaDeTareasR);
-
+           
